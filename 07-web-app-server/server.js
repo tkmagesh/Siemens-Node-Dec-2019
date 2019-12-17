60dfb6c71f346e8c5d208449eaf5a159b6db224c
Magesh Kuppan <tkmagesh77@gmail.com>
2019-12-17 10:00:41 +0530

    Handling POST requests

--- 07-web-app-server/server.js
+++ 07-web-app-server/server.js
@@ -12,9 +12,8 @@ function isStatic(resourceName){
 
 var server = http.createServer(function connectionHandler(req, res) {
     var urlObj = url.parse(req.url),
-        resourceName = urlObj.pathname;
-    console.log(req.method + '\t' + resourceName);
-
+        resourceName = urlObj.pathname === '/' ? '/index.html' : urlObj.pathname;
+    console.log(req.method + '\t' + urlObj.pathname);
     if (isStatic(resourceName)){
         var resourceFullName = path.join(__dirname, resourceName);
         if (!fs.existsSync(resourceFullName)){
@@ -23,7 +22,7 @@ var server = http.createServer(function connectionHandler(req, res) {
             return;
         }
         fs.createReadStream(resourceFullName).pipe(res);
-    } else if (urlObj.pathname === '/calculator') {
+    } else if (urlObj.pathname === '/calculator' && req.method === 'GET') {
         var queryData = querystring.parse(urlObj.query);
         var op = queryData.op,
             x = parseInt(queryData.x),
@@ -31,6 +30,20 @@ var server = http.createServer(function connectionHandler(req, res) {
             result = calculator[op](x, y);
         res.write(result.toString());
         res.end();
+    } else if (urlObj.pathname === '/calculator' && req.method === 'POST') {
+        var rawData = '';
+        req.on('data', function(chunk){
+            rawData += chunk;
+        });
+        req.on('end', function(){
+            var bodyData = querystring.parse(rawData);
+            var op = bodyData.op,
+                x = parseInt(bodyData.x),
+                y = parseInt(bodyData.y),
+                result = calculator[op](x, y);
+            res.write(result.toString());
+            res.end();
+        })
     } else {
         res.statusCode = 404;
         res.end();

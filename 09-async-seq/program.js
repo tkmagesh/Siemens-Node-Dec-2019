4f8ed616ad0b8bb3bb2307268c62f5e96959e550
Magesh Kuppan <tkmagesh77@gmail.com>
2019-12-17 12:00:53 +0530

    async - seq - assignment - solution

--- 09-async-seq/program.js
+++ 09-async-seq/program.js
@@ -18,48 +18,77 @@ function f4Sync() {
     console.log('f4Sync completed');
 }
 
+var syncFns = [ f1Sync, f2Sync, f3Sync, f4Sync ];
+
 function runSync(){
-    f1Sync();
-    f2Sync();
-    f3Sync();
-    f4Sync();
+    for(var i=0; i < syncFns.length; i++){
+        var syncFn = syncFns[i];
+        syncFn();
+    }
 }
 
 module.exports['runSync'] = runSync;
 
-function f1Async() {
+function f1Async(next) {
     console.log('f1Async started');
     setTimeout(function(){
         console.log('f1Async completed');
+        if (typeof next === 'function')
+            next();
     }, 4000);
 }
 
-function f2Async() {
+function f2Async(next) {
     console.log('f2Async started');
     setTimeout(function(){
         console.log('f2Async completed');
+        if (typeof next === 'function')
+            next();
     }, 2000);
 }
 
-function f3Async() {
+function f3Async(next) {
     console.log('f3Async started');
     setTimeout(function(){
         console.log('f3Async completed');
+        if (typeof next === 'function')
+            next();
     }, 5000);
 }
 
-function f4Async() {
+function f4Async(next) {
     console.log('f4Async started');
     setTimeout(function(){
         console.log('f4Async completed');
+        if (typeof next === 'function')
+            next();
     }, 3000);
 }
 
+var asyncFns = [f1Async, f2Async, f3Async, f4Async ];
+
+/* function exec(){
+    f1Async(function(){
+        f2Async(function(){
+            f3Async(function(){
+                f4Async();
+            });
+        });
+    });
+} */
+
+function exec(fns){
+    var first = fns[0],
+        remaining = fns.slice(1),
+        next = function(){
+            exec(remaining);
+        };
+    if (typeof first === 'function')
+        first(next);
+}
+
 function runAsync() {
-    f1Async();
-    f2Async();
-    f3Async();
-    f4Async();
+   exec(asyncFns);
 }
 
 module.exports['runAsync'] = runAsync;
